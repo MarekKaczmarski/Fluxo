a0985cad4c0aa8500376b7609cff7a291faf39cc
Marek Kaczmarski <marconayt@gmail.com>
2026-02-03 23:50:30 +0100

    feat(domain): add Transaction entity

---
+++ Fluxo.Domain/Entities/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluxo.Domain.Entities
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public required string Category { get; set; }
+
+        // Powiązanie z kontem (uproszczone na start)
+        public Guid AccountId { get; set; }
+    }
+}
