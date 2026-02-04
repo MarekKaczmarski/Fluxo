2c810a0772cc857cd7f3c04f2a2a128cff7b0b10
Marek Kaczmarski <marconayt@gmail.com>
2026-02-04 15:35:38 +0100

    feat(infrastructure): add FluxoDbContext with Fluent API configuration

---
+++ Fluxo.Infrastructure/Data/FluxoDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Fluxo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fluxo.Infrastructure.Data;
+
+public class FluxoDbContext : DbContext
+{
+    public FluxoDbContext(DbContextOptions<FluxoDbContext> options) : base(options) { }
+
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Transaction>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(e => e.Amount)
+                .HasColumnType("decimal(18,2)");
+
+            entity.Property(e => e.Category)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity.Property(e => e.Date)
+                .IsRequired();
+        });
+    }
+}
