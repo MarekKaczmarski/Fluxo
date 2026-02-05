504fb3604f62c1a6afd06b2a5ea8136ceb4c5a1d
Marek Kaczmarski <marconayt@gmail.com>
2026-02-05 23:51:24 +0100

    feat(api): add TransactionsController with direct handler injection

---
+++ Fluxo.Api/Controllers/TransactionsController.cs
@@ -0,0 +1,21 @@
+﻿using Fluxo.Application.Transactions.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TransactionsController : ControllerBase
+{
+    private readonly ICreateTransactionHandler _handler;
+
+    public TransactionsController(ICreateTransactionHandler handler)
+    {
+        _handler = handler;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Guid>> Create(CreateTransactionCommand command)
+    {
+        var result = await _handler.Handle(command, default);
+        return Ok(result);
+    }
+}
