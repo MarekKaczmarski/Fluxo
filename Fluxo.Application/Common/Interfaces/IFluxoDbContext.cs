6dfbc2cfc46eb9344a37b8c71622043cdad63d13
Marek Kaczmarski <marconayt@gmail.com>
2026-02-05 23:54:25 +0100

    feat(application): add IFluxoDbContext interface for dependency inversion

---
+++ Fluxo.Application/Common/Interfaces/IFluxoDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Fluxo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fluxo.Application.Common.Interfaces;
+
+public interface IFluxoDbContext
+{
+    DbSet<Transaction> Transactions { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
