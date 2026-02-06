7009f16f7603a515d66dcddb6b7fdb85ede0a508
Marek Kaczmarski <marconayt@gmail.com>
2026-02-06 16:22:09 +0100

    feat(application): add FluentValidation for CreateTransactionCommand

---
+++ Fluxo.Application/Transactions/Commands/CreateTransaction/CreateTransactionCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace Fluxo.Application.Transactions.Commands.CreateTransaction;
+
+public class CreateTransactionCommandValidator : AbstractValidator<CreateTransactionCommand>
+{
+    public CreateTransactionCommandValidator()
+    {
+        RuleFor(v => v.Title)
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(100).WithMessage("Title must not exceed 100 characters.");
+
+        RuleFor(v => v.Amount)
+            .GreaterThan(0).WithMessage("Amount must be greater than zero.");
+
+        RuleFor(v => v.Category)
+            .NotEmpty().WithMessage("Category is required.")
+            .MaximumLength(50).WithMessage("Category must not exceed 50 characters.");
+
+        RuleFor(v => v.Date)
+            .NotEmpty().WithMessage("Date is required.")
+            .LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Date cannot be in the future.");
+
+        RuleFor(v => v.AccountId)
+            .NotEmpty().WithMessage("AccountId is required.");
+    }
+}
