66572e873810429ebbe408906ff1022a8a65b947
Marek Kaczmarski <marconayt@gmail.com>
2026-02-06 17:38:54 +0100

    refactor(application): move ICreateTransactionHandler to a separate file

---
+++ Fluxo.Application/Transactions/Commands/CreateTransaction/ICreateTransactionHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluxo.Application.Transactions.Commands.CreateTransaction
+{
+    public interface ICreateTransactionHandler
+    {
+        Task<Guid> Handle(CreateTransactionCommand command, CancellationToken ct);
+    }
+}
