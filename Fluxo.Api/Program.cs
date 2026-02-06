706928f757adf17de964cd50c1f3a3dbbccaf247
Marek Kaczmarski <marconayt@gmail.com>
2026-02-06 21:12:56 +0100

    fix(api): add Swashbuckle package and fix Swagger middleware configuration

--- Fluxo.Api/Program.cs
+++ Fluxo.Api/Program.cs
@@ -1,48 +1,41 @@
+using FluentValidation;
+using Fluxo.Application.Common.Interfaces;
 using Fluxo.Application.Transactions.Commands.CreateTransaction;
 using Fluxo.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<FluxoDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IFluxoDbContext>(provider =>
+    provider.GetRequiredService<FluxoDbContext>());
 builder.Services.AddScoped<ICreateTransactionHandler, CreateTransactionHandler>();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateTransactionCommandValidator>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+if (app.Environment.IsDevelopment())
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fluxo API V1");
+        c.RoutePrefix = "swagger";
+    });
+}
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
