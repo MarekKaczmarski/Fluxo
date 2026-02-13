56a7acc0f909c2193858aa4ca069cd7989444c95
Marek Kaczmarski <marconayt@gmail.com>
2026-02-14 00:47:07 +0100

    feat(api): add PUT endpoint for transaction updates

--- Fluxo.Api/Controllers/TransactionsController.cs
+++ Fluxo.Api/Controllers/TransactionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Fluxo.Application.Transactions.Commands.CreateTransaction;
 using Fluxo.Application.Transactions.Commands.DeleteTransaction;
+using Fluxo.Application.Transactions.Commands.UpdateTransaction;
 using Fluxo.Application.Transactions.Queries.GetTransactions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 public class TransactionsController(
     IGetTransactionsHandler getHandler,
     ICreateTransactionHandler createHandler,
+    IUpdateTransactionHandler updateHandler,
     IDeleteTransactionHandler deleteHandler) 
     : ControllerBase
 {
@@ -19,10 +21,23 @@ public class TransactionsController(
     }
 
     [HttpPost]
-    public async Task<ActionResult<Guid>> Create(CreateTransactionCommand command)
+    public async Task<ActionResult<Guid>> Create(CreateTransactionCommand command, CancellationToken ct)
     {
         var id = await createHandler.Handle(command, default);
         return Ok(id);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult> Update(Guid id, UpdateTransactionCommand command, CancellationToken ct)
+    {
+        if (id != command.Id)
+        {
+            return BadRequest("ID in URL does not match ID in body.");
+        }
+
+        await updateHandler.Handle(command, ct);
+
+        return NoContent();
     }
 
     [HttpDelete]
