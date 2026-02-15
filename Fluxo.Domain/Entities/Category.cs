9cd5a5409ba2d59011b3793a03ec96877529cdee
Marek Kaczmarski <marconayt@gmail.com>
2026-02-15 23:59:08 +0100

    feat(domain): add Category entity

---
+++ Fluxo.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluxo.Domain.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Icon { get; set; }
+
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
