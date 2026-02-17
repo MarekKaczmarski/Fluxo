e512a9006d252113c2947e6ef439fd255f2ff650
Marek Kaczmarski <marconayt@gmail.com>
2026-02-17 23:59:08 +0100

    feat(api): add CategoriesController for retrieving category list

---
+++ Fluxo.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using Fluxo.Application.Categories.Queries.GetCategories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fluxo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController(IGetCategoriesHandler handler) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetCategories(CancellationToken ct)
+        {
+            var result = await handler.HandleAsync(new GetCategoriesQuery(), ct);
+            return Ok(result);
+        }
+    }
+}
