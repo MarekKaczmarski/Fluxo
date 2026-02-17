0f59956870b570152901ccef47c55d1abf0f2a76
Marek Kaczmarski <marconayt@gmail.com>
2026-02-18 00:01:26 +0100

    feat(application): add Categories DbSet to IFluxoDbContext interface

--- Fluxo.Application/Common/Interfaces/IFluxoDbContext.cs
+++ Fluxo.Application/Common/Interfaces/IFluxoDbContext.cs
@@ -6,5 +6,6 @@ namespace Fluxo.Application.Common.Interfaces;
 public interface IFluxoDbContext
 {
     DbSet<Transaction> Transactions { get; }
+    DbSet<Category> Categories { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
