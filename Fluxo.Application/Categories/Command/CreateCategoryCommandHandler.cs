46eb6ed25b2f8a71efd2d3cd709ea5e35e187e27
Marek Kaczmarski <marconayt@gmail.com>
2026-02-20 23:58:47 +0100

    feat(application): add Create command handlers for categories

---
+++ Fluxo.Application/Categories/Command/CreateCategoryCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Fluxo.Application.Common.Interfaces;
+using Fluxo.Domain.Entities;
+using Fluxo.Infrastructure.Data;
+
+namespace Fluxo.Application.Categories.Commands.CreateCategory;
+
+public class CreateCategoryHandler(FluxoDbContext context) : ICreateCategoryHandler
+{
+    public async Task<Guid> HandleAsync(CreateCategoryCommand command, CancellationToken ct)
+    {
+        var category = new Category
+        {
+            Id = Guid.NewGuid(),
+            Name = command.Name,
+            Icon = command.Icon,
+            Color = command.Color
+        };
+
+        context.Categories.Add(category);
+        await context.SaveChangesAsync(ct);
+
+        return category.Id;
+    }
+}
