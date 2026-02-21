fb1b61e6eb12f85661587cc9a2316859014ebb5f
Marek Kaczmarski <marconayt@gmail.com>
2026-02-21 23:15:47 +0100

    feat(api): expand CategoriesController with Create action

--- Fluxo.Api/Controllers/CategoriesController.cs
+++ Fluxo.Api/Controllers/CategoriesController.cs
@@ -1,17 +1,29 @@
-﻿using Fluxo.Application.Categories.Queries.GetCategories;
+﻿using Fluxo.Application.Categories.Command;
+using Fluxo.Application.Categories.Queries.GetCategories;
+using Fluxo.Application.Transactions.Commands.CreateTransaction;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fluxo.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CategoriesController(IGetCategoriesHandler handler) : ControllerBase
+    public class CategoriesController(
+        IGetCategoriesHandler getHandler,
+        ICreateCategoryHandler createHandler) 
+        : ControllerBase
     {
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CategoryDto>>> GetCategories(CancellationToken ct)
         {
-            var result = await handler.HandleAsync(new GetCategoriesQuery(), ct);
+            var result = await getHandler.HandleAsync(new GetCategoriesQuery(), ct);
             return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> CreateCategory(CreateCategoryCommand command, CancellationToken ct)
+        {
+            var id = await createHandler.HandleAsync(command, default);
+            return Ok(id);
         }
     }
 }
