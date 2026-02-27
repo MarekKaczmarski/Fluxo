842393c9b79c5e3874e6d906878adc709dee4595
Marek Kaczmarski <marconayt@gmail.com>
2026-02-27 23:15:47 +0100

    feat(domain): create Account entity

---
+++ Fluxo.Domain/Entities/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluxo.Domain.Entities
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public decimal Balance { get; set; }
+        public string Currency { get; set; } = "";
+
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
