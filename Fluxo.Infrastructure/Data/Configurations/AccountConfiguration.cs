a96f07d5878e568658de0d34321ed5f68b7508e7
Marek Kaczmarski <marconayt@gmail.com>
2026-03-03 23:20:25 +0100

    feat(infrastructure): configure Account entity mapping

---
+++ Fluxo.Infrastructure/Data/Configurations/AccountConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Fluxo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fluxo.Infrastructure.Data.Configurations;
+
+public class AccountConfiguration : IEntityTypeConfiguration<Account>
+{
+    public void Configure(EntityTypeBuilder<Account> builder)
+    {
+        builder.HasKey(a => a.Id);
+
+        builder.Property(a => a.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(a => a.Currency)
+            .IsRequired()
+            .HasMaxLength(3);
+
+        builder.HasMany(a => a.Transactions)
+            .WithOne(t => t.Account)
+            .HasForeignKey(t => t.AccountId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
