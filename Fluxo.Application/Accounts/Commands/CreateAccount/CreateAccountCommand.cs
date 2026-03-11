e8f07ef2cf0f5068a36102d6f745965bcfa70d2c
Marek Kaczmarski <marconayt@gmail.com>
2026-03-11 14:11:02 +0100

    feat(app): add CreateAccountCommand record

---
+++ Fluxo.Application/Accounts/Commands/CreateAccount/CreateAccountCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace Fluxo.Application.Accounts.Commands.CreateAccount;
+
+public record CreateAccountCommand(string Name, string? Description, decimal InitialBalance, string Currency = "PLN");
