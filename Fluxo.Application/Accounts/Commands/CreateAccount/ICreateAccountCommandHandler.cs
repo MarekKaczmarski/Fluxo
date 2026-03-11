56850d46d495934dda5abd60115a8fbddd84daec
Marek Kaczmarski <marconayt@gmail.com>
2026-03-11 14:11:22 +0100

    feat(app): add ICreateAccountCommandHandler interface

---
+++ Fluxo.Application/Accounts/Commands/CreateAccount/ICreateAccountCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluxo.Application.Accounts.Commands.CreateAccount
+{
+    public interface ICreateAccountCommandHandler
+    {
+        Task<Guid> HandleAsync(CreateAccountCommand command, CancellationToken ct);
+    }
+}
