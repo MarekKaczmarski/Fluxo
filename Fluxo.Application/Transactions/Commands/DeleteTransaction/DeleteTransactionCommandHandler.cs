c14b220154ee0233b6b04f8460f9cfbcbe33e5d6
Marek Kaczmarski <marconayt@gmail.com>
2026-03-21 23:51:43 +0100

    feat(app): implement balance reversal in DeleteTransactionCommandHandler

--- Fluxo.Application/Transactions/Commands/DeleteTransaction/DeleteTransactionCommandHandler.cs
+++ Fluxo.Application/Transactions/Commands/DeleteTransaction/DeleteTransactionCommandHandler.cs
@@ -1,5 +1,6 @@
-﻿using Fluxo.Application.Exceptions;
-using Fluxo.Application.Common.Interfaces;
+﻿using Fluxo.Application.Common.Interfaces;
+using Fluxo.Application.Exceptions;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,12 +11,16 @@ namespace Fluxo.Application.Transactions.Commands.DeleteTransaction
     {
         public async Task HandleAsync(DeleteTransactionCommand command, CancellationToken ct)
         {
-            var transaction = await context.Transactions.FindAsync(command.Id);
+            var transaction = await context.Transactions
+                .Include(t => t.Account)
+                .FirstOrDefaultAsync(t => t.Id == command.Id, ct);
 
             if (transaction is null)
             {
                 throw new NotFoundException($"Transaction with ID {command.Id} was not found.");
             }
+
+            transaction.Account.Balance -= transaction.Amount;
 
             context.Transactions.Remove(transaction);
             await context.SaveChangesAsync(ct);
