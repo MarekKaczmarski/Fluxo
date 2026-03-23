f56065128cc0d0cf401898cdfde92abd7f015672
Marek Kaczmarski <marconayt@gmail.com>
2026-03-23 21:29:48 +0100

    feat(infra): add AccountSeeder with default test account

---
+++ Fluxo.Infrastructure/Data/Seeding/AccountSeeder.cs
@@ -0,0 +1,20 @@
+﻿using Fluxo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluxo.Infrastructure.Data.Seeding;
+public static class AccountSeeder
+{
+    public static IEnumerable<Account> GetSeedData() => new List<Account>
+    {
+        new()
+        {
+            Id = SeedDataConstants.DefaultAccountId,
+            Name = "Main Wallet",
+            Description = "Default personal cash wallet",
+            Balance = 5000.00m,
+            Currency = "PLN"
+        }
+    };
+}
