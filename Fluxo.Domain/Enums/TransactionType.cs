01bbcc4f1e9714114da26bf10e8dd3ae9dd3c712
Marek Kaczmarski <marconayt@gmail.com>
2026-05-15 03:00:56 +0200

    feat(domain): add TransactionType enum for income and expense classification

---
+++ Fluxo.Domain/Enums/TransactionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluxo.Domain.Enums
+{
+    public enum TransactionType
+    {
+        Expense = 0,
+        Income = 1
+    }
+}
