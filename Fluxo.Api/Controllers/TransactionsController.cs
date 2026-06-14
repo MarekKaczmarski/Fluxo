fa7eb7832bb82ea708c39129f7902d71300d190d
Marek Kaczmarski <marconayt@gmail.com>
2026-06-14 23:46:10 +0200

    refactor(api): add TransactionsController namespace

--- Fluxo.Api/Controllers/TransactionsController.cs
+++ Fluxo.Api/Controllers/TransactionsController.cs
@@ -4,46 +4,49 @@ using Fluxo.Application.Transactions.Commands.UpdateTransaction;
 using Fluxo.Application.Transactions.Queries.GetTransactions;
 using Microsoft.AspNetCore.Mvc;
 
-[ApiController]
-[Route("api/[controller]")]
-public class TransactionsController(
-    IGetTransactionsQueryHandler getHandler,
-    ICreateTransactionCommandHandler createHandler,
-    IUpdateTransactionCommandHandler updateHandler,
-    IDeleteTransactionCommandHandler deleteHandler)
-    : ControllerBase
+namespace Fluxo.Api.Controllers
 {
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<TransactionDto>>> GetTransactions(CancellationToken ct)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionsController(
+        IGetTransactionsQueryHandler getHandler,
+        ICreateTransactionCommandHandler createHandler,
+        IUpdateTransactionCommandHandler updateHandler,
+        IDeleteTransactionCommandHandler deleteHandler)
+        : ControllerBase
     {
-        var result = await getHandler.HandleAsync(new GetTransactionsQuery(), ct);
-        return Ok(result);
-    }
-
-    [HttpPost]
-    public async Task<ActionResult<Guid>> CreateTransaction(CreateTransactionCommand command, CancellationToken ct)
-    {
-        var id = await createHandler.HandleAsync(command, ct);
-        return Ok(id);
-    }
-
-    [HttpPut("{id:guid}")]
-    public async Task<ActionResult> UpdateTransaction(Guid id, UpdateTransactionCommand command, CancellationToken ct)
-    {
-        if (id != command.Id)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TransactionDto>>> GetTransactions(CancellationToken ct)
         {
-            return BadRequest("ID in URL does not match ID in body.");
+            var result = await getHandler.HandleAsync(new GetTransactionsQuery(), ct);
+            return Ok(result);
         }
 
-        await updateHandler.HandleAsync(command, ct);
+        [HttpPost]
+        public async Task<ActionResult<Guid>> CreateTransaction(CreateTransactionCommand command, CancellationToken ct)
+        {
+            var id = await createHandler.HandleAsync(command, ct);
+            return Ok(id);
+        }
 
-        return NoContent();
-    }
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult> UpdateTransaction(Guid id, UpdateTransactionCommand command, CancellationToken ct)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest("ID in URL does not match ID in body.");
+            }
 
-    [HttpDelete("{id:guid}")]
-    public async Task<ActionResult> DeleteTransaction(Guid id, CancellationToken ct)
-    {
-        await deleteHandler.HandleAsync(new DeleteTransactionCommand(id), ct);
-        return NoContent();
+            await updateHandler.HandleAsync(command, ct);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> DeleteTransaction(Guid id, CancellationToken ct)
+        {
+            await deleteHandler.HandleAsync(new DeleteTransactionCommand(id), ct);
+            return NoContent();
+        }
     }
 }
