dc0619a25a88e4d8cc7803786cadf25407c9615b
Marek Kaczmarski <marconayt@gmail.com>
2026-06-17 23:54:47 +0200

    feat(api): migrate swagger to scalar and configure structured logging with serilog

--- Fluxo.Api/Program.cs
+++ Fluxo.Api/Program.cs
@@ -2,68 +2,97 @@ using FluentValidation;
 using Fluxo.Api.Exceptions;
 using Fluxo.Application.Common.Interfaces;
 using Fluxo.Application.Transactions.Commands.CreateTransaction;
-using Fluxo.Application.Transactions.Queries.GetTransactions;
 using Fluxo.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-var builder = WebApplication.CreateBuilder(args);
+using Scalar.AspNetCore;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
-
-builder.Services.AddDbContext<FluxoDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IFluxoDbContext>(provider =>
-    provider.GetRequiredService<FluxoDbContext>());
-//builder.Services.AddScoped<IGetTransactionsHandler, GetTransactionsHandler>();
-//builder.Services.AddScoped<ICreateTransactionHandler, CreateTransactionHandler>();
-builder.Services.AddValidatorsFromAssemblyContaining<CreateTransactionCommandValidator>();
-
-builder.Services.Scan(scan => scan
-    .FromAssemblies(
-        typeof(ICreateTransactionCommandHandler).Assembly,
-        typeof(FluxoDbContext).Assembly)
-    .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Handler")))
-    .AsImplementedInterfaces()
-    .WithScopedLifetime());
-
-builder.Services.AddCors(options =>
+try
 {
-    options.AddPolicy("AllowVueClient", policy =>
+    Log.Information("Starting up");
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((_, __, configuration) => configuration
+        .MinimumLevel.Information()
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Information)
+        .Enrich.FromLogContext()
+        .Enrich.WithExceptionDetails()
+        .WriteTo.Console(
+            outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+            theme: Serilog.Sinks.SystemConsole.Themes.AnsiConsoleTheme.Code
+        ));
+
+    builder.Services.AddOpenApi();
+    builder.Services.AddControllers();
+    builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+    builder.Services.AddProblemDetails();
+
+    builder.Services.AddDbContext<FluxoDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddScoped<IFluxoDbContext>(provider =>
+        provider.GetRequiredService<FluxoDbContext>());
+    //builder.Services.AddScoped<IGetTransactionsHandler, GetTransactionsHandler>();
+    //builder.Services.AddScoped<ICreateTransactionHandler, CreateTransactionHandler>();
+
+    builder.Services.AddValidatorsFromAssemblyContaining<CreateTransactionCommandValidator>();
+
+    builder.Services.Scan(scan => scan
+        .FromAssemblies(
+            typeof(ICreateTransactionCommandHandler).Assembly,
+            typeof(FluxoDbContext).Assembly)
+        .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Handler")))
+        .AsImplementedInterfaces()
+        .WithScopedLifetime());
+
+    builder.Services.AddCors(options =>
     {
-        policy.WithOrigins("http://localhost:5173")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowAnyOrigin();
+        options.AddPolicy("AllowVueClient", policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin();
+        });
     });
-});
 
-var app = builder.Build();
 
-app.UseExceptionHandler();
+    var app = builder.Build();
 
-app.UseCors("AllowVueClient");
+    app.UseExceptionHandler();
+    app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
+    app.UseRouting();
+    app.UseCors("AllowVueClient");
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
+    if (app.Environment.IsDevelopment())
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fluxo API V1");
-        c.RoutePrefix = "swagger";
-    });
+        app.MapOpenApi();
+        app.MapScalarApiReference();
+    }
+
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
