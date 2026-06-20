d7002e1f88a9d571f321e9e18319ad713ecd299f
Marek Kaczmarski <marconayt@gmail.com>
2026-06-21 00:08:10 +0200

    chore(router): setup routes for dashboard, transactions, categories and accounts

--- Fluxo.Client/fluxo/src/router/index.ts
+++ Fluxo.Client/fluxo/src/router/index.ts
@@ -1,8 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AccountsView from '@/views/AccountsView.vue'
+import CategoriesView from '@/views/CategoriesView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import TransactionsView from '@/views/TransactionsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      component: DashboardView,
+      path: '/',
+    },
+    {
+      component: TransactionsView,
+      path: '/transactions',
+    },
+    {
+      component: CategoriesView,
+      path: '/categories',
+    },
+    {
+      component: AccountsView,
+      path: '/accounts',
+    },
+  ],
 })
 
 export default router
