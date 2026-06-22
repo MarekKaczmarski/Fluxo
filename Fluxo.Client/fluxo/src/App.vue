6618be24e83540868d685df07f1d4d7d0d4bf724
Marek Kaczmarski <marconayt@gmail.com>
2026-06-22 22:59:01 +0200

    feat(client): integrate global AppShell layout into root App component

--- Fluxo.Client/fluxo/src/App.vue
+++ Fluxo.Client/fluxo/src/App.vue
@@ -1,31 +1,7 @@
-<script setup lang="ts">
-import TransactionManager from './components/TransactionManager.vue'
-</script>
-
 <template>
-  <main class="app-container">
-    <header class="app-header">
-      <h1>Fluxo App</h1>
-    </header>
-
-    <TransactionManager />
-  </main>
+  <AppShell />
 </template>
 
-<style scoped>
-.app-container {
-  max-width: 800px;
-  margin: 2rem auto;
-  padding: 0 1rem;
-  font-family: sans-serif;
-}
-.app-header {
-  border-bottom: 2px solid #eee;
-  margin-bottom: 2rem;
-  padding-bottom: 0.5rem;
-}
-h1 {
-  color: #333;
-  margin: 0;
-}
-</style>
+<script setup lang="ts">
+import AppShell from './components/AppShell.vue'
+</script>
