90804519e2107beaa146ef8afd99294c7c1c55b7
Marek Kaczmarski <marconayt@gmail.com>
2026-06-22 23:00:56 +0200

    feat(client): implement core AppShell structure

---
+++ Fluxo.Client/fluxo/src/components/AppShell.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app class="app-shell">
+    <RouterView />
+    <MobileNav />
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import MobileNav from './MobileNav.vue'
+</script>
+
+<style scoped>
+.app-shell {
+  min-height: 100vh;
+}
+</style>
