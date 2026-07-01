5ba127adc5ead92d877d9824b31207c95b9c1786
Marek Kaczmarski <marconayt@gmail.com>
2026-07-01 23:11:34 +0200

    refactor(application/accounts): replace dbcontext with uniqueness checker in update account validator

--- Fluxo.Application/Accounts/Commands/UpdateAccount/UpdateAccountCommandValidator.cs
+++ Fluxo.Application/Accounts/Commands/UpdateAccount/UpdateAccountCommandValidator.cs
@@ -1,29 +1,35 @@
 using FluentValidation;
 using Fluxo.Application.Common.Interfaces;
-using Microsoft.EntityFrameworkCore;
 
-namespace Fluxo.Application.Accounts.Commands.UpdateAccount;
-
-public class UpdateAccountCommandValidator : AbstractValidator<UpdateAccountCommand>
+namespace Fluxo.Application.Accounts.Commands.UpdateAccount
 {
-    private readonly IFluxoDbContext _context;
-
-    public UpdateAccountCommandValidator(IFluxoDbContext context)
+    public class UpdateAccountCommandValidator : AbstractValidator<UpdateAccountCommand>
     {
-        _context = context;
+        public UpdateAccountCommandValidator(IAccountUniquenessChecker uniquenessChecker)
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Account ID is required.");
 
-        RuleFor(x => x.Id)
-            .NotEmpty().WithMessage("Account ID is required.");
+            RuleFor(x => x.Name)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage("Account name is required.")
+                .MaximumLength(100)
+                .WithMessage("Account name is too long.")
+                .MustAsync(
+                    (command, name, ct) =>
+                        BeUniqueNameAsync(uniquenessChecker, command.Id, name, ct)
+                )
+                .WithMessage("Account with this name already exists.");
+        }
 
-        RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Account name is required.")
-            .MaximumLength(100).WithMessage("Account name is too long.")
-            .MustAsync(BeUniqueName).WithMessage("Account with this name already exists.");
-    }
-
-    private async Task<bool> BeUniqueName(UpdateAccountCommand command, string name, CancellationToken ct)
-    {
-        return !await _context.Accounts
-            .AnyAsync(a => a.Id != command.Id && a.Name.ToLower() == name.ToLower(), ct);
+        private static async Task<bool> BeUniqueNameAsync(
+            IAccountUniquenessChecker checker,
+            Guid currentId,
+            string name,
+            CancellationToken ct
+        )
+        {
+            return !await checker.IsNameTakenAsync(name, excludeId: currentId, ct);
+        }
     }
 }
