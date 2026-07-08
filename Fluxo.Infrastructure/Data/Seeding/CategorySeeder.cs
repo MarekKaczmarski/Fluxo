1f43427804649f6e698a2dc5fcd33fee9d18565d
Marek Kaczmarski <marconayt@gmail.com>
2026-07-08 20:46:07 +0200

    refactor(application): simplify GetSeedData method in CategorySeeder

--- Fluxo.Infrastructure/Data/Seeding/CategorySeeder.cs
+++ Fluxo.Infrastructure/Data/Seeding/CategorySeeder.cs
@@ -1,19 +1,18 @@
-﻿using Fluxo.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Fluxo.Domain.Entities;
 
-namespace Fluxo.Infrastructure.Data.Seeding
+namespace Fluxo.Infrastructure.Data.Seeding;
+
+public static class CategorySeeder
 {
-    public static class CategorySeeder
+    public static IEnumerable<Category> GetSeedData()
     {
-        public static IEnumerable<Category> GetSeedData() => new List<Category>
-        {
+        return
+        [
             new Category(SeedDataConstants.TransferCategoryId, "Transfer", "wallet"),
             new Category(SeedDataConstants.FoodCategoryId, "Food", "utensils"),
             new Category(SeedDataConstants.ElectronicsCategoryId, "Electronics", "smartphone"),
             new Category(SeedDataConstants.PharmacyCategoryId, "Pharmacy", "pill"),
-            new Category(SeedDataConstants.TransportCategoryId, "Transport", "bus")
-        };
+            new Category(SeedDataConstants.TransportCategoryId, "Transport", "bus"),
+        ];
     }
 }
