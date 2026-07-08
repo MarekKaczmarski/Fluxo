2523da4cc5b1a28c0e5c056b1f3e176b2e9bbd43
Marek Kaczmarski <marconayt@gmail.com>
2026-07-08 19:16:36 +0200

    feat(application): add GetTransactionById query and handler

--- Fluxo.Api/Controllers/TransactionsController.cs
+++ Fluxo.Api/Controllers/TransactionsController.cs
@@ -1,57 +1,68 @@
 using Fluxo.Application.Transactions.Commands.CreateTransaction;
 using Fluxo.Application.Transactions.Commands.DeleteTransaction;
 using Fluxo.Application.Transactions.Commands.UpdateTransaction;
+using Fluxo.Application.Transactions.Queries.GetTransactionById;
 using Fluxo.Application.Transactions.Queries.GetTransactions;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Fluxo.Api.Controllers
+namespace Fluxo.Api.Controllers;
+
+[Route("api/[controller]")]
+public class TransactionsController(
+    IGetTransactionsQueryHandler getHandler,
+    IGetTransactionByIdQueryHandler getByIdHandler,
+    ICreateTransactionCommandHandler createHandler,
+    IUpdateTransactionCommandHandler updateHandler,
+    IDeleteTransactionCommandHandler deleteHandler
+) : ApiControllerBase
 {
-    [Route("api/[controller]")]
-    public class TransactionsController(
-        IGetTransactionsQueryHandler getHandler,
-        ICreateTransactionCommandHandler createHandler,
-        IUpdateTransactionCommandHandler updateHandler,
-        IDeleteTransactionCommandHandler deleteHandler
-    ) : ApiControllerBase
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TransactionDto>>> GetTransactions(
+        CancellationToken ct
+    )
     {
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<TransactionDto>>> GetTransactions(
-            CancellationToken ct
-        )
-        {
-            var result = await getHandler.HandleAsync(new GetTransactionsQuery(), ct);
-            return Ok(result);
-        }
+        var result = await getHandler.HandleAsync(new GetTransactionsQuery(), ct);
+        return Ok(result);
+    }
 
-        [HttpPost]
-        public async Task<ActionResult<Guid>> CreateTransaction(
-            CreateTransactionCommand command,
-            CancellationToken ct
-        )
-        {
-            var id = await createHandler.HandleAsync(command, ct);
-            return CreatedAtAction(nameof(GetTransactions), new { id }, id);
-        }
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<TransactionDto>> GetTransactionById(
+        Guid id,
+        CancellationToken ct
+    )
+    {
+        var transaction = await getByIdHandler.HandleAsync(new GetTransactionByIdQuery(id), ct);
+        return Ok(transaction);
+    }
 
-        [HttpPut("{id:guid}")]
-        public async Task<ActionResult> UpdateTransaction(
-            Guid id,
-            UpdateTransactionCommand command,
-            CancellationToken ct
-        )
-        {
-            if (EnsureMatchingId(id, command.Id) is { } error)
-                return error;
+    [HttpPost]
+    public async Task<ActionResult<Guid>> CreateTransaction(
+        CreateTransactionCommand command,
+        CancellationToken ct
+    )
+    {
+        var id = await createHandler.HandleAsync(command, ct);
+        return CreatedAtAction(nameof(GetTransactionById), new { id }, id);
+    }
 
-            await updateHandler.HandleAsync(command, ct);
-            return NoContent();
-        }
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult> UpdateTransaction(
+        Guid id,
+        UpdateTransactionCommand command,
+        CancellationToken ct
+    )
+    {
+        if (EnsureMatchingId(id, command.Id) is { } error)
+            return error;
 
-        [HttpDelete("{id:guid}")]
-        public async Task<ActionResult> DeleteTransaction(Guid id, CancellationToken ct)
-        {
-            await deleteHandler.HandleAsync(new DeleteTransactionCommand(id), ct);
-            return NoContent();
-        }
+        await updateHandler.HandleAsync(command, ct);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> DeleteTransaction(Guid id, CancellationToken ct)
+    {
+        await deleteHandler.HandleAsync(new DeleteTransactionCommand(id), ct);
+        return NoContent();
     }
 }
