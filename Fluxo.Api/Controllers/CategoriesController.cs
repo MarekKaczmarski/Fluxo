6a25b3d2d7cc3ea2c35ba69a0186b4a31899ebec
Marek Kaczmarski <marconayt@gmail.com>
2026-07-08 19:17:45 +0200

    refactor(application/categories): standardize CategoriesController structure and improve route definitions

--- Fluxo.Api/Controllers/CategoriesController.cs
+++ Fluxo.Api/Controllers/CategoriesController.cs
@@ -2,56 +2,62 @@ using Fluxo.Application.Categories.Commands.CreateCategory;
 using Fluxo.Application.Categories.Commands.DeleteCategory;
 using Fluxo.Application.Categories.Commands.UpdateCategory;
 using Fluxo.Application.Categories.Queries.GetCategories;
+using Fluxo.Application.Categories.Queries.GetCategoryById;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Fluxo.Api.Controllers
+namespace Fluxo.Api.Controllers;
+
+[Route("api/[controller]")]
+public class CategoriesController(
+    IGetCategoriesQueryHandler getHandler,
+    IGetCategoryByIdQueryHandler getByIdHandler,
+    ICreateCategoryCommandHandler createHandler,
+    IUpdateCategoryCommandHandler updateHandler,
+    IDeleteCategoryCommandHandler deleteHandler
+) : ApiControllerBase
 {
-    [Route("api/[controller]")]
-    public class CategoriesController(
-        IGetCategoriesHandler getHandler,
-        ICreateCategoryCommandHandler createHandler,
-        IUpdateCategoryCommandHandler updateHandler,
-        IDeleteCategoryCommandHandler deleteHandler
-    ) : ApiControllerBase
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CategoryDto>>> GetCategories(CancellationToken ct)
     {
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetCategories(
-            CancellationToken ct
-        )
-        {
-            var result = await getHandler.HandleAsync(new GetCategoriesQuery(), ct);
-            return Ok(result);
-        }
+        var result = await getHandler.HandleAsync(new GetCategoriesQuery(), ct);
+        return Ok(result);
+    }
 
-        [HttpPost]
-        public async Task<ActionResult<Guid>> CreateCategory(
-            CreateCategoryCommand command,
-            CancellationToken ct
-        )
-        {
-            var id = await createHandler.HandleAsync(command, ct);
-            return CreatedAtAction(nameof(GetCategories), new { id }, id);
-        }
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<CategoryDto>> GetCategoryById(Guid id, CancellationToken ct)
+    {
+        var category = await getByIdHandler.HandleAsync(new GetCategoryByIdQuery(id), ct);
+        return Ok(category);
+    }
 
-        [HttpPut("{id:guid}")]
-        public async Task<IActionResult> UpdateCategory(
-            Guid id,
-            UpdateCategoryCommand command,
-            CancellationToken ct
-        )
-        {
-            if (EnsureMatchingId(id, command.Id) is { } error)
-                return error;
+    [HttpPost]
+    public async Task<ActionResult<Guid>> CreateCategory(
+        CreateCategoryCommand command,
+        CancellationToken ct
+    )
+    {
+        var id = await createHandler.HandleAsync(command, ct);
+        return CreatedAtAction(nameof(GetCategoryById), new { id }, id);
+    }
 
-            await updateHandler.HandleAsync(command, ct);
-            return NoContent();
-        }
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult> UpdateCategory(
+        Guid id,
+        UpdateCategoryCommand command,
+        CancellationToken ct
+    )
+    {
+        if (EnsureMatchingId(id, command.Id) is { } error)
+            return error;
 
-        [HttpDelete("{id:guid}")]
-        public async Task<IActionResult> DeleteCategory(Guid id, CancellationToken ct)
-        {
-            await deleteHandler.HandleAsync(new DeleteCategoryCommand(id), ct);
-            return NoContent();
-        }
+        await updateHandler.HandleAsync(command, ct);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> DeleteCategory(Guid id, CancellationToken ct)
+    {
+        await deleteHandler.HandleAsync(new DeleteCategoryCommand(id), ct);
+        return NoContent();
     }
 }
